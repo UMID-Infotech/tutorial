6685a55d48eeb8529ca4a222aca9b3d774f849d8
Aksshay-spec <umidphp.akshay@.com>
2026-04-09 11:59:05 +0530

    base URL updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://tutorial-rlnv.onrender.com",
+  // baseURL: "http://localhost:4000/api",
 });
 
 // Attach token automatically
