16ca1fe5c8751b16a1cd4dfb125cd617ae5ed98a
Aksshay-spec <umidphp.akshay@.com>
2026-04-15 13:19:10 +0530

    api.js  updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 // Attach token automatically
